f10a2b41e39f6d7bbc8a61e836d2ce90b6ac8b9b
blakeNaccarato <blake.naccarato@gmail.com>
2022-09-10 16:17:15 -0700

    Try this

--- docs/conf.py
+++ docs/conf.py
@@ -76,41 +76,25 @@ ogp_custom_meta_tags = [
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-myst_enable_extensions = [
-    "dollarmath",
-    "amsmath",
-    "deflist",
-    "fieldlist",
-    "html_admonition",
-    "html_image",
-    "colon_fence",
-    "smartquotes",
-    "replacements",
-    "linkify",
-    "strikethrough",
-    "substitution",
-    "tasklist",
-]
+# myst_enable_extensions = [
+#     "dollarmath",
+#     "amsmath",
+#     "deflist",
+#     "fieldlist",
+#     "html_admonition",
+#     "html_image",
+#     "colon_fence",
+#     "smartquotes",
+#     "replacements",
+#     "linkify",
+#     "strikethrough",
+#     "substitution",
+#     "tasklist",
+# ]
 myst_number_code_blocks = ["typescript"]
 myst_heading_anchors = 2
 myst_footnote_transition = True
 myst_dmath_double_inline = True
-
-rediraffe_redirects = {
-    "using/intro.md": "sphinx/intro.md",
-    "sphinx/intro.md": "intro.md",
-    "using/use_api.md": "api/index.md",
-    "api/index.md": "api/reference.rst",
-    "using/syntax.md": "syntax/syntax.md",
-    "using/syntax-optional.md": "syntax/optional.md",
-    "using/reference.md": "syntax/reference.md",
-    "sphinx/reference.md": "configuration.md",
-    "sphinx/index.md": "faq/index.md",
-    "sphinx/use.md": "faq/index.md",
-    "sphinx/faq.md": "faq/index.md",
-    "explain/index.md": "develop/background.md",
-}
-
 suppress_warnings = ["myst.strikethrough"]
 
 
@@ -119,39 +103,6 @@ intersphinx_mapping = {
     "sphinx": ("https://www.sphinx-doc.org/en/master", None),
     "markdown_it": ("https://markdown-it-py.readthedocs.io/en/latest", None),
 }
-
-# autodoc_default_options = {
-#     "show-inheritance": True,
-#     "special-members": "__init__, __enter__, __exit__",
-#     "members": True,
-#     # 'exclude-members': '',
-#     "undoc-members": True,
-#     # 'inherited-members': True
-# }
-autodoc_member_order = "bysource"
-nitpicky = True
-nitpick_ignore = [
-    ("py:class", "docutils.nodes.document"),
-    ("py:class", "docutils.nodes.docinfo"),
-    ("py:class", "docutils.nodes.Element"),
-    ("py:class", "docutils.nodes.Node"),
-    ("py:class", "docutils.nodes.field_list"),
-    ("py:class", "docutils.nodes.problematic"),
-    ("py:class", "docutils.nodes.pending"),
-    ("py:class", "docutils.nodes.system_message"),
-    ("py:class", "docutils.statemachine.StringList"),
-    ("py:class", "docutils.parsers.rst.directives.misc.Include"),
-    ("py:class", "docutils.parsers.rst.Parser"),
-    ("py:class", "docutils.utils.Reporter"),
-    ("py:class", "nodes.Element"),
-    ("py:class", "nodes.Node"),
-    ("py:class", "nodes.system_message"),
-    ("py:class", "Directive"),
-    ("py:class", "Include"),
-    ("py:class", "StringList"),
-    ("py:class", "DocutilsRenderer"),
-    ("py:class", "MockStateMachine"),
-]
 
 
 def setup(app: Sphinx):
--- docs/faq/snippets/include-rst.rst
+++
@@ -1,1 +0,0 @@
-Hallo I'm from an rST file, :ref:`with a reference <howto/autodoc>`.
